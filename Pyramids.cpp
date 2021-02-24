ad3c4d7f6c1d2ba91fb5033eaab1fc52587598d2
Muhammad Bilal Gulzar <79194902+Bilal-731@users.noreply.github.com>
2021-02-24 11:47:10 +0500

    Add files via upload

---
+++ Pyramids.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+	int rows;
+	
+	cout<<"Enter the number of rows= ";
+	
+	cin>>rows;
+	
+	for(int a=1; a>=1; ++a)
+	
+	{
+	for (int b=1; b>=1; ++b)
+	{
+	cout<<"7";
+	}
+	cout<<"\n";
+	}
+}
